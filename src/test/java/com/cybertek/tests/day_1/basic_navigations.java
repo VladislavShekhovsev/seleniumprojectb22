f4e1e3e499bb7524802c2dafe8be33b36a62c846
Vladislav <vladislav13457@gmail.com>
2021-05-09 02:04:30 -0400

    uploading my Selenium project to github

--- src/test/java/com/cybertek/tests/day_1/basic_navigations.java
+++ src/test/java/com/cybertek/tests/day_1/basic_navigations.java
@@ -8,22 +8,37 @@ public class basic_navigations {
     public static void main(String[] args) throws InterruptedException {
         //1)-set-up the browser driver
         WebDriverManager.chromedriver().setup();
+
         //2)- create instance of selenium web driver
         WebDriver driver= new ChromeDriver();
+
         driver.get("http://www.tesla.com");
+
+        System.out.println("current title "+driver.getTitle());
+
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println(currentUrl);
+
         //putting 3 seconds off sleep to delay the go back
         Thread.sleep(3000);
+
         //going back using navigation
         driver.navigate().back();
+
         // going forward using navigation
         Thread.sleep(3000);
+
         driver.navigate().forward();
+
         // refreshing the page using navigation
         Thread.sleep(3000);
         driver.navigate().refresh();
+
         // going to some other website using navigation
         driver.navigate().to("http://www.google.com");
+
         // provide the title of the web page and return Sting title
         System.out.println("current title "+driver.getTitle());
+
     }
 }
